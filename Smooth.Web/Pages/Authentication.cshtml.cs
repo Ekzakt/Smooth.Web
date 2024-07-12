cde1945ee742eb6f51b8b9b780e0c1cd41e025e1
EricJansen <EricJansen@LAPTOP-EJ>
2024-07-11 21:13:21 -0400

    Displaying data in Authentication page.

--- Smooth.Web/Pages/Authentication.cshtml.cs
+++ Smooth.Web/Pages/Authentication.cshtml.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Smooth.Web.Pages;
@@ -6,7 +8,16 @@ namespace Smooth.Web.Pages;
 [Authorize]
 public class AuthenticationModel : PageModel
 {
-    public void OnGet()
+    [BindProperty]
+    public AuthenticationProperties? AuthenticationProperties { get; set; }
+
+    public async Task OnGet()
     {
+        var result = await HttpContext.AuthenticateAsync();
+
+        if (result.Succeeded)
+        {
+            AuthenticationProperties = result.Properties;
+        }
     }
 }
